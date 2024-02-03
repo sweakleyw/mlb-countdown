bf3ae1e7f0671cb6d50f5b556b7c283f128389ea
sweakleyw <sweakleyworks@gmail.com>
2024-02-02 21:58:54 -0500

    change

--- mlb-count/src/api/index.jsx
+++ mlb-count/src/api/index.jsx
@@ -1,15 +1,6 @@
 const BASE_URL =
   "https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBGamesForDate?gameDate=20240328";
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "0ec3e974bemsh7fc5ac38d3012bep1fcb4cjsn32908b6a2ffa",
-    "X-RapidAPI-Host":
-      "tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com",
-  },
-};
-
 export async function getGames() {
   try {
     const response = await fetch(BASE_URL, options);
